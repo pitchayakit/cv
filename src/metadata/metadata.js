d14d372a21ecda2084e96dbfa9227530456096c4
Pitchayakit <pitchayakit.p@gmail.com>
2021-10-09 11:40:19 +0700

    Update content

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,105 @@
 module.exports = {
   name: 'Pitchayakit Pahamak',
-  title: 'Poet, playwright and actor',
+  title: 'Software Engineer',
   facts: {
-    'Residence': 'London',
-    'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <span class="print">https://en.wikipedia.org/wiki/William_Shakespeare</span>
-                  <span class="screen">wiki</span>
-                </a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com">wil64@yahoo.com</a>'
+    'Residence': 'Bangkok, Thailand',
+    'Phone': '<a href="tel:+66831225471"><i class="fa fa-phone" aria-hidden="true"></i> +66831225471</a>',
+    'GitHub': '<a href="https://github.com/pitchayakit"><i class="fa fa-github" aria-hidden="true"></i>pitchayakit</a>',
+    'Email': '<a href="mailto:pitchayakit.p@gmail.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>pitchayakit.p@gmail.com</a>'
   },
   skills: [
-    ['Comedy', '++++'],
-    ['Love', '++++'],
-    ['Theater', '++++'],
-    ['Tragedy', '++++'],
-    ['Poetry', '++++'],
-    ['Erotic', '+++'],
-    ['Sonnets', '++++'],
-    ['Plays', '++++'],
-    ['Writing', '++++'],
-    ['Directing', '+++'],
-    ['Literature', '++++'],
-    ['Influence', '++++'],
+    ['Laravel', '++++'],
+    ['PHP', '++++'],
+    ['SCSS', '++++'],
+    ['jQuery', '++++'],
+    ['Boostrab 4', '++++'],
+    ['Tailwind', '++++'],
+    ['WordPress', '++++'],
+    ['Resful API', '++++'],
+    ['GIT', '++++'],
+    ['SQL', '++++'],
+    ['C', '++++'],
+    ['Alpine JS', '+++'],
+    ['VueJs', '+++'],
+    ['NodeJs', '+++'],
+    ['Linux', '+++'],
+    ['Python', '++'],
+    
   ],
   plays: [
     {
-      title: 'Hamlet',
-      written: 'Likely early 17th century',
-      badges: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: 'Lexicon',
+      written: '2020 - prosent',
+      badges: ['Laravel', 'WordPress', 'VueJS', 'Nova', 'Alpine JS'],
+      contents: [
+        'Managed up Line CRM system', 
+        'Managed up CMS using Laravel', 
+        'Electric search', 
+        'Data migration', 
+        'Coaching internships in web development and technical support'
+      ]
     },
     {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      title: 'Freelancer',
+      written: '2018 – 2020',
+      badges: ['WordPress', 'Laravel'],
+      contents: [
+        'Managed up to 5 company websites', 
+        'Managed up to 2 E-commerce websites', 
+        '1 library website for searching books using Laravel'
+      ]
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
-    }
+      title: 'Empag',
+      written: '2017 – 2018',
+      badges: ['WordPress', 'NodeJs'],
+      contents: [
+        'Coaching internships in web development and technical support', 
+        'Managed up to 4 web content management systems and tasks at a given time', 
+        'Collaborate with designer in order to ensure quality and accuracy of the designy', 
+        'A Support tool for web scraping and export to excel file'
+      ]
+    },
+    {
+      title: 'Onestockhome',
+      written: 'Mar 2017 – Sep 2017',
+      badges: ['Ruby on Rail', 'React'],
+      contents: [
+        'Migration content management system to Ruby on Rail platform', 
+        'Developed front-end website using jQuery, Boostrap, HTML and CSS in Ruby on Rail platform', 
+        'Supported the use of content management system and improve user interface'
+      ]
+    },
+    {
+      title: 'Rippleworkz',
+      written: '2016 – 2017',
+      badges: ['WordPress', 'jQuery'],
+      contents: [
+        'Integrated HTML to WordPress and Prestashop', 
+        'Managed up to 4 webs content management system and tasks at a given time.', 
+        'Collaborate with project manager to develop website, which is beyond own management project'
+      ]
+    },
+    {
+      title: 'SeSaMe (Sensor-enhanced Social Media) Center, National University of Singapore',
+      written: 'Jul 2015 – Dec 2015',
+      badges: ['Play Framework', 'HTML Canvas', 'Joomla', 'OpenCV'],
+      contents: [
+        'SENSE video analytics web application - replaced video output for detection, tracking and counting by HTML Canvas', 
+        'Take over and developed SeSaMe website using Joomla CMS', 
+      ]
+    },
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      header: 'A Study of using IoT and Zenbo robot to facilitate students’ interaction in flipped classroom and the analysis of their perception.',
+      skills: ['IoT', 'Zenbo', 'Machine learning '],
+      contents: ``
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+      header: 'Smart home with door and light control System.',
+      skills: ['IoT', 'PHP', 'jQuery'],
+      contents: ``
     }
   ]
 };
